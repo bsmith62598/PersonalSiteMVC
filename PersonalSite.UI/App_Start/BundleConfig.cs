eba8b730f8d5da165a9a71d938773331067744a6
bsmith62598 <bsmith62598@gmail.com>
2021-06-14 18:34:38 -0500

    Nearly finished setting up layout and index page

--- PersonalSite.UI/App_Start/BundleConfig.cs
+++ PersonalSite.UI/App_Start/BundleConfig.cs
@@ -24,7 +24,8 @@ namespace PersonalSite.UI
 
             bundles.Add(new StyleBundle("~/Content/css").Include(
                       "~/Content/bootstrap.css",
-                      "~/Content/site.css"));
+                      "~/Content/site.css",
+                      "~/Content/main.css"));
         }
     }
 }
